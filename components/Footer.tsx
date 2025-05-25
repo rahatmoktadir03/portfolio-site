46c4535e69578978a9682e983cb21d1354bd1dae
Rahat Moktadir <rahatmoktadir03@gmail.com>
2025-05-25 00:43:12 -0400

    completed the footer by fixing my social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -38,13 +37,17 @@ const Footer = () => {
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+          {socialMedia.map((profile) => (
+            <a key={profile.id} href={profile.link} target="_blank">
+              <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+                <img
+                  src={profile.img}
+                  alt={`${profile.id}`}
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </a>
           ))}
         </div>
       </div>
